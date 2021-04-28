78d82da656af8a59d6673be6c6d60d9649d6ba2a
Yan <yancarlosrocha@gmail.com>
2021-04-28 15:08:15 -0300

    Create Entities and relations

---
+++ src/main/java/entity/Author.java
@@ -0,0 +1,26 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private Integer age;
+
+}
